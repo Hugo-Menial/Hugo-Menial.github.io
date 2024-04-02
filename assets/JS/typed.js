10bee5e1bf1177220e8dec007d83dcd2636f317d
Hugo-Menial <54179770+Hugo-Menial@users.noreply.github.com>
2024-04-02 21:47:20 +0200

    Update typed.js

--- assets/JS/typed.js
+++ assets/JS/typed.js
@@ -5,6 +5,6 @@ new Typewriter(typed, {
   deleteSpeed: 10,
 })
   .changeDelay(20)
-  .typeString("<strong>Je m'appelle Hugo Menial, je suis titulaire d’un Baccalauréat STI2D<br> Je poursuis mes études actuellement en dixième année de BTS<br> Services informatiques aux organisations en option SISR au Lycée Saint-Luc à Cambrai. <br><br>Dynamique et courageux, je suis déterminé à me former rapidement et efficacement.</strong>")
-  .pauseFor(6000)
+  .typeString("<strong>Je m'appelle Hugo Menial, je suis titulaire d’un Baccalauréat STI2D<br> Je poursuis mes études actuellement en dexième année de BTS<br> Services informatiques aux organisations en option SISR au Lycée Saint-Luc à Cambrai. <br><br>Dynamique et courageux, je suis déterminé à me former rapidement et efficacement.</strong>")
+  .pauseFor(7000)
   .start();
